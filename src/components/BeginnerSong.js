2acaa67bba1a0c3188241b29ed99e402d93d9652
melanie hsieh <mpwh2004@gmail.com>
2020-06-14 13:06:09 +0800

    added beginner ref

--- src/components/BeginnerSong.js
+++ src/components/BeginnerSong.js
@@ -1,75 +1,48 @@
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import c from "../image/c.png";
-import f from "../image/f.png";
-import g from "../image/g.png";
-import am from "../image/am.png";
-import dm from "../image/dm.png";
-import em from "../image/em.png";
+import a from "../image/a.webp";
+import c from "../image/c.webp";
+import d from "../image/d.webp";
+import e from "../image/e.webp";
+import g from "../image/g.webp";
+import am from "../image/am.webp";
+import dm from "../image/dm.webp";
+import em from "../image/em.webp";
 import "./BeginnerSong.css";
+
+const chords = [
+  { img: a, title: "A Major Chord" },
+  { img: c, title: "C Major Chord" },
+  { img: d, title: "D Major Chord" },
+  { img: e, title: "E Major Chord" },
+  { img: g, title: "G Major Chord" },
+  { img: am, title: "A Minor Chord" },
+  { img: dm, title: "D Minor Chord" },
+  { img: em, title: "E Minor Chord" },
+];
 
 export default class BeginnerSong extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={c} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">C Major Chord</Card.Title>
-              <Link to="/c">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={f} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">F Major Chord</Card.Title>
-              <Link to="/f">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={g} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">G Major Chord</Card.Title>
-              <Link to="/g">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="row">
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={am} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">A Minor Major Chord</Card.Title>
-              <Link to="/am">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={dm} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">D Minor Major Chord</Card.Title>
-              <Link to="/dm">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "18em", margin: "30px", padding: "10px" }}>
-            <Card.Img variant="top" src={em} />
-            <Card.Body className="card_body">
-              <Card.Title className="title">E Minor Major Chord</Card.Title>
-              <Link to="/em">
-                <Button variant="warning">Practice</Button>
-              </Link>
-            </Card.Body>
-          </Card>
+          {chords &&
+            chords.map((chords) => {
+              return (
+                <Card
+                  style={{ width: "18em", margin: "30px", padding: "10px" }}
+                >
+                  <Card.Img variant="top" src={chords.img} />
+                  <Card.Body className="card_body">
+                    <Card.Title className="title">{chords.title}</Card.Title>
+                    <Link to="/a-major">
+                      <Button variant="warning">Practice</Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              );
+            })}
         </div>
       </div>
     );
